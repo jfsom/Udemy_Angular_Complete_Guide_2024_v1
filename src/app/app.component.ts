b8ac310bafab17d5a97af2dbc7189ceff7ae307f
jfsom <vijay.jfsom@gmail.com>
2024-02-01 21:38:56 +0530

    Update styles and add server component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h1{
+    color: red;
+  }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'my-first-appkkk';
 }
